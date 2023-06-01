65de06b0ee204a7cc6cd6a7b59307649a0eb713c
Meedoriya <meedoriya@gmail.com>
2023-06-01 21:37:26 +0500

    first commit

---
+++ src/main/java/com/alibi/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,56 @@
+package com.alibi.spring.rest.controller;
+
+import com.alibi.spring.rest.entity.Employee;
+import com.alibi.spring.rest.exception_handling.NoSuchEmployeeException;
+import com.alibi.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no such employee with id " + id + " in our data base");
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with id " + id);
+        } else {
+            employeeService.deleteEmployee(id);
+            return "We deleted info about employee with id " + id;
+        }
+    }
+}
---
+++ src/main/java/com/alibi/spring/rest/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.alibi.spring.rest.service;
+
+import com.alibi.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
